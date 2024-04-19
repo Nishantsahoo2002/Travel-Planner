2b0ba6aaf14ecac5d30e75256bcee713fecde5ce
SHISHIR1973 <shishirkumar7330@gmail.com>
2024-04-20 00:05:58 +0530

    pay

---
+++ payment/config.php
@@ -0,0 +1,18 @@
+<?php
+
+$keyId = 'rzp_test_64tZaUr6ptzv55';
+$keySecret = 'Do052o5wETQhdzouICmmiOUa';
+$displayCurrency = 'INR';
+
+//These should be commented out in production
+// This is for error reporting
+// Add it to config.php to report any errors
+// error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+
+
+//DATABASE CONNECTION DETAILS
+$host = "127.0.0.1:3307";
+$username = "tpp";
+$password = "Tpp@2324";
+$dbname = "travelplanner";
